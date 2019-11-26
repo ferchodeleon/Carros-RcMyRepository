971a25f6e1e1ef36e0974cbb86aeb8abacb9076e
Fernando De Leon Gil <30879564+ferchodeleon@users.noreply.github.com>
2019-11-26 15:21:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php session_start();
-if (isset($_SESSION['usuario'])) {
-	header('Location: PHP/rol.php');
-} else {
-	header('Location: PHP/inicio-sesion.php');
-}
+	if (isset($_SESSION['usuario'])) {
+		header('Location: PHP/rol.php');
+	} else {
+		header('Location: PHP/inicio-sesion.php');
+	}
 ?>
